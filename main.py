de4f2e859d450d60ac46d4073b7594b557158999
Ugur <uguralgul23@gmail.com>
2022-10-06 09:57:22 +0200

    fix

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import discord
+from discord.commands import Option
 
-bot = discord.Bot()
+intent = discord.Intents.default()
+intent.message_content = True
 
-bot.run("")
+bot = discord.Bot(intents = intent)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} ist Online!")
+
+@bot.slash_command(description="Grüße einen User")
+async def greet(ctx, user: Option(discord.Member, "Der User, den du Grüßen willst"))
+
+bot.run("MTAyNzQ4NzUwNDQzNTI2NTU5Nw.G2-v03.5S-oaA2U0eKO7ozG1NiswfldD01zm9TFLk0Ztg")
